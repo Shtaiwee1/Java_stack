ea4ea3c6a0ca75eabb01d7526363b1b52fe89c9f
Shtaiwee <shtaiweeomair@gmail.com>
2022-03-12 14:19:26 +0200

    calculator assignment

--- Java_OOP/Calculator/CalculatorTest.java
+++ Java_OOP/Calculator/CalculatorTest.java
@@ -4,8 +4,8 @@ public class CalculatorTest {
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
-        calculator.setNum1(2.5);
-        calculator.setNum2(3.5);
+        calculator.setNum1(2);
+        calculator.setNum2(3);
         calculator.setOperation("-");
         calculator.performOperation();
         calculator.getResult();
