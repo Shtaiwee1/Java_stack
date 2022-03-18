479e43a09389fc8fb2f132ba5b65a7eddb65bf6c
Shtaiwee <shtaiweeomair@gmail.com>
2022-03-18 11:23:43 +0200

    done

---
+++ Spring_MVC/Edit_expenses/SaveTravelsMVC/src/main/java/com/savetravelsmvc/services/ExpenseService.java
@@ -0,0 +1,49 @@
+package com.savetravelsmvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.savetravelsmvc.models.Expense;
+import com.savetravelsmvc.repositories.ExpenseRepository;
+
+
+@Service
+public class ExpenseService {
+	
+	private final ExpenseRepository expenseRepository;
+	 
+	 public ExpenseService(ExpenseRepository expenseRepository) {
+	     this.expenseRepository = expenseRepository;
+	 }
+	 // returns all the expenses
+	 public List<Expense> allExpenses() {
+	     return expenseRepository.findAll();
+	 }
+	 // create a new expense
+	 public Expense createExpense(Expense e) {
+	     return expenseRepository.save(e);
+	 }
+	 
+	// retrieves an expense
+		public Expense findExpense(Long id) {
+			Optional<Expense> optionalExpense = expenseRepository.findById(id);
+			if (optionalExpense.isPresent()) {
+				return optionalExpense.get();
+			} else {
+				return null;
+			}
+		}
+	 
+	//Updates an Expense
+	 public Expense updateExpense(Long id, Expense expense) {
+		 Expense e = findExpense(id);
+			e.setName(expense.getName());
+			e.setVendor(expense.getVendor());
+			e.setAmount(expense.getAmount());
+			e.setDescription(expense.getDescription());
+			return expenseRepository.save(e);
+		}
+
+}
